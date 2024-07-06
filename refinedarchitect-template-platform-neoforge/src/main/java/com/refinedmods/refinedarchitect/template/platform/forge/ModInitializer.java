e94095280bc76b8c5658505ec1d12f53d2638349
raoulvdberge <raoulvdberge@gmail.com>
2024-07-06 18:42:45 +0200

    style: checkstyle issues

--- refinedarchitect-template-platform-neoforge/src/main/java/com/refinedmods/refinedarchitect/template/platform/forge/ModInitializer.java
+++ refinedarchitect-template-platform-neoforge/src/main/java/com/refinedmods/refinedarchitect/template/platform/forge/ModInitializer.java
@@ -1,6 +1,7 @@
 package com.refinedmods.refinedarchitect.template.platform.forge;
 
 import com.refinedmods.refinedarchitect.template.platform.common.Common;
+
 import net.neoforged.api.distmarker.Dist;
 import net.neoforged.bus.api.IEventBus;
 import net.neoforged.bus.api.SubscribeEvent;
