93b5177860267dceaeb6b4becf9316cc7b90c7f5
raoulvdberge <raoulvdberge@gmail.com>
2024-09-10 17:11:59 +0200

    chore: fix configurations
    Needed for new refined architect

--- refinedarchitect-template-neoforge/build.gradle.kts
+++ refinedarchitect-template-neoforge/build.gradle.kts
@@ -5,7 +5,6 @@ plugins {
 refinedarchitect {
     modId = "refinedarchitecttemplate"
     neoForge()
-    compileWithProject(project(":refinedarchitect-template-common"))
     publishing {
         // maven = true
         // curseForge = "123"
@@ -15,4 +14,13 @@ refinedarchitect {
 
 base {
     archivesName.set("refinedarchitect-template-neoforge")
+}
+
+val commonJava by configurations.existing
+val commonResources by configurations.existing
+
+dependencies {
+    compileOnly(project(":refinedarchitect-template-common"))
+    commonJava(project(path = ":refinedarchitect-template-common", configuration = "commonJava"))
+    commonResources(project(path = ":refinedarchitect-template-common", configuration = "commonResources"))
 }
